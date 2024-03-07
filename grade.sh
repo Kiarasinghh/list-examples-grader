e26317b56e162b5fd08bfeaea64257edf4bfaa9e
Kiara Singh <156370071+Kiarasinghh@users.noreply.github.com>
2024-03-07 12:52:25 -0800

    Add files via upload

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -16,18 +16,43 @@ else
 fi 
 
 cp student-submission/ListExamples.java grading-area/
-cp TestListExamples.java grading-area/ 
+cp TestListExamples.java grading-area/
 cp -r lib grading-area
 
 cd grading-area
 
-javac -cp $CPATH *.java
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
 
-if [$? !=0]; then
+if [ $? -ne 0 ]; then
     echo complication error 
     exit 1
 fi
+
 echo program compiled successfully 
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples >JunitOutput.txt 2>&1
+ls .
+cat JunitOutput.txt
+
+grep "Tests run:" JunitOutput.txt  > testsRun.txt
+
+if grep -q "OK" JunitOutput.txt; then
+    echo "100%! Good job"
+else
+    # Extracting numbers using grep and awk
+    test_run=$(grep -o 'Tests run: [0-9]*' testsRun.txt | awk '{print $3}')
+    failures=$(grep -o 'Failures: [0-9]*' testsRun.txt | awk '{print $2}')
+
+    # Calculating percentage
+    if [[ $test_run -ne 0 ]]; then
+        pass_percentage=$((passes * 100 / test_run))
+    else
+        pass_percentage=0
+    fi
+fi
+
+echo "Percentage of passing tests: $pass_percentage%"
+
 
 
 # Draw a picture/take notes on the directory structure that's set up after
